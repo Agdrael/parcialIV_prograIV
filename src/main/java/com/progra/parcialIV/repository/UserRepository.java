8e7a352d8667e69e44b65cc9d12b6daa17511935
Agdrael <rodrigojoserivera2016@gmail.com>
2025-11-23 08:10:05 -0600

    agregando repositorio

---
+++ src/main/java/com/progra/parcialIV/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.progra.parcialIV.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.progra.parcialIV.model.Usuario;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Usuario,Long> {
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+    boolean exiexistsByNombreUsuario(String nombreUsuario);
+}
