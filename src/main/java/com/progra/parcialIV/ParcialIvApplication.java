b68a3ea9a071390b8959cefc8e8141f8dff8d4d0
Agdrael <rodrigojoserivera2016@gmail.com>
2025-11-23 07:33:08 -0600

    base del proyecto

---
+++ src/main/java/com/progra/parcialIV/ParcialIvApplication.java
@@ -0,0 +1,13 @@
+package com.progra.parcialIV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialIvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progra/parcialIV/ParcialIvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progra.parcialIV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialIvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
